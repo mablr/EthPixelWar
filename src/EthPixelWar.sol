5ae068649cb54eecd1d230c88fdcb430c4ea507b
mablr <mablr@pm.me>
2025-01-29 22:08:03 +0100

    clean import

--- src/EthPixelWar.sol
+++ src/EthPixelWar.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract EthPixelWar is Ownable {
     struct Pixel {
